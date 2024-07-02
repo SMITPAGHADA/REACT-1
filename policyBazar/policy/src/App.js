0679c9ea4baae933e73a210062a2f9e6a35bcfda
SMITPAGHADA <smitpagada40@gmail.com>
2024-07-02 11:43:19 +0530

    test

---
+++ policyBazar/policy/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+
+
+function App() {
+  return (
+<>
+
+<About/>
+{/* <Home/> */}
+
+</>
+  );
+}
+
+export default App;
