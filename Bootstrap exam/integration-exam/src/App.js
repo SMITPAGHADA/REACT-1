c909bc675c58a6f0eb80d2098593a37922692589
SMITPAGHADA <smitpagada40@gmail.com>
2024-07-12 14:48:41 +0530

    test

---
+++ Bootstrap exam/integration-exam/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Btemplet from './component/Btemplet';
+
+function App() {
+  return (
+    <>
+    <Btemplet/>
+    </>
+  );
+}
+
+export default App;
